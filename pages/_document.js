a80ef1ebe0c93ba8c1e480c947f6f70de81f1d88
Renpinning <irene@rti.org.tw>
2026-01-28 10:41:16 +0800

    增加GA4代碼

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,26 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="zh-TW">
-      <Head />
+      <Head>
+        {/* 1. 載入 Google tag (gtag.js) 外部腳本 */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-XDE3PHY0TX"
+        />
+        {/* 2. 初始化 gtag */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-XDE3PHY0TX', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
